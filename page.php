fe1232f794a8c2ded5cce5245dab7e57d9bda4ba
Alejo <70706948+VictiniFan360@users.noreply.github.com>
2025-09-20 17:30:00 +0200

    Solving page issue

---
+++ page.php
@@ -0,0 +1,33 @@
+<?php
+get_header(); 
+?>
+
+<main id="main-content" class="content-area">
+
+    <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+
+            <article <?php post_class('frame-container'); ?>>
+
+                <h1 class="entry-title"><?php the_title(); ?></h1>
+
+                <div class="entry-content">
+                    <?php the_content();?>
+                </div>
+
+            </article>
+
+        <?php endwhile; ?>
+    <?php else : ?>
+
+        <div class="frame-container" style="text-align:center;">
+            <p><?php esc_html_e('No se encontró la página.', 'pokemon-theme'); ?></p>
+        </div>
+
+    <?php endif; ?>
+
+</main>
+
+<?php
+get_footer();
+?>
